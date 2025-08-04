70a347e947a31ea5e5720ed5204fe7b41f6d5028
mattletford <90183213+mattletford@users.noreply.github.com>
2025-08-04 20:36:25 +0100

    add coin details, chart, loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
-import CoinCard from './components/CoinCard';
-import LimitSelector from './components/LimitSelector';
-import FilterInput from './components/FilterInput';
-import SortSelector from './components/SortSelector';
-const API_URL = import.meta.env.VITE_API_URL;
+import { Routes, Route } from 'react-router';
+import Header from './components/Header';
+import HomePage from './pages/home';
+import AboutPage from './pages/about';
+import NotFoundPage from './pages/not-found';
+import CoinDetailsPage from './pages/coin-details';
+const COINS_API_URL = import.meta.env.VITE_COINS_API_URL;
 
 const App = () => {
     const [coins, setCoins] = useState([]);
@@ -17,7 +19,7 @@ const App = () => {
         const fetchCoins = async () => {
             try {
                 const res = await fetch(
-                    `${API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
+                    `${COINS_API_URL}&order=market_cap_desc&per_page=${limit}&page=1&sparkline=false`
                 );
                 if (!res.ok) throw new Error('Failed to fetch data');
                 const data = await res.json();
@@ -33,61 +35,31 @@ const App = () => {
         fetchCoins();
     }, [limit]);
 
-    const filteredCoins = coins
-        .filter((coin) => {
-            return (
-                coin.name.toLowerCase().includes(filter.toLowerCase()) ||
-                coin.symbol.toLowerCase().includes(filter.toLowerCase())
-            );
-        })
-        .slice()
-        .sort((a, b) => {
-            switch (sortBy) {
-                case 'market_cap_desc':
-                    return b.market_cap - a.market_cap;
-                case 'market_cap_asc':
-                    return a.market_cap - b.market_cap;
-                case 'price_desc':
-                    return b.current_price - a.current_price;
-                case 'price_asc':
-                    return a.current_price - b.current_price;
-                case 'change_desc':
-                    return (
-                        b.price_change_percentage_24h -
-                        a.price_change_percentage_24h
-                    );
-                case 'change_asc':
-                    return (
-                        a.price_change_percentage_24h -
-                        b.price_change_percentage_24h
-                    );
-            }
-        });
-
     return (
-        <div>
-            <h1>Crypto Dash</h1>
-            {loading && <p>Loading...</p>}
-            {error && <div className="error">{error}</div>}
-
-            <div className="top-controls">
-                <LimitSelector limit={limit} onLimitChange={setLimit} />
-                <FilterInput filter={filter} onFilterChange={setFilter} />
-                <SortSelector sortBy={sortBy} onSortChange={setSortBy} />
-            </div>
-
-            {!loading && !error && (
-                <main className="grid">
-                    {filteredCoins.length > 0 ? (
-                        filteredCoins.map((coin) => (
-                            <CoinCard key={coin.id} coin={coin} />
-                        ))
-                    ) : (
-                        <p>No matching coins</p>
-                    )}
-                </main>
-            )}
-        </div>
+        <>
+            <Header />
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <HomePage
+                            coins={coins}
+                            filter={filter}
+                            setFilter={setFilter}
+                            limit={limit}
+                            setLimit={setLimit}
+                            sortBy={sortBy}
+                            setSortBy={setSortBy}
+                            loading={loading}
+                            error={error}
+                        />
+                    }
+                />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/coin/:id" element={<CoinDetailsPage />} />
+                <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+        </>
     );
 };
 
---
+++ src/pages/about.jsx
@@ -0,0 +1,29 @@
+const AboutPage = () => {
+    return (
+        <div className="about">
+            <h1>About Crypto Dash</h1>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Doloremque modi nesciunt aperiam, placeat nemo soluta repellat
+                velit ipsa ducimus dicta!
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Doloremque modi nesciunt aperiam, placeat nemo soluta repellat
+                velit ipsa ducimus dicta!
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Doloremque modi nesciunt aperiam, placeat nemo soluta repellat
+                velit ipsa ducimus dicta!
+            </p>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Doloremque modi nesciunt aperiam, placeat nemo soluta repellat
+                velit ipsa ducimus dicta!
+            </p>
+        </div>
+    );
+};
+
+export default AboutPage;
